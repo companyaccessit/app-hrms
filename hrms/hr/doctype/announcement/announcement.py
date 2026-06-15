845c84c20f63afc9e8ecfc97b7609bf34b9a0be7
dbpr <company.access.it@gmail.com>
2026-06-15 08:01:46 +0000

    update anouncement

--- hrms/hr/doctype/announcement/announcement.py
+++ hrms/hr/doctype/announcement/announcement.py
@@ -14,12 +14,18 @@ class Announcement(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		amended_from: DF.Link | None
+		announcement_by: DF.Data | None
+		approved_date: DF.Date | None
 		company: DF.Link | None
 		content: DF.TextEditor | None
 		end_date: DF.Date | None
-		objective: DF.Data | None
-		request_announcement: DF.Data | None
+		is_approved: DF.Check
+		memo: DF.Data | None
+		posting_date: DF.Date
+		reference: DF.Data | None
 		start_date: DF.Date | None
+		title: DF.Data
 	# end: auto-generated types
 
 	pass
