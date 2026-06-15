970909eaafcc9ce3132910cf43ed7d5ba7979177
dbpr <company.access.it@gmail.com>
2026-06-15 07:02:02 +0000

    add new doctype - announcement

---
+++ hrms/hr/doctype/announcement/announcement.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Announcement(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		company: DF.Link | None
+		content: DF.TextEditor | None
+		end_date: DF.Date | None
+		objective: DF.Data | None
+		request_announcement: DF.Data | None
+		start_date: DF.Date | None
+	# end: auto-generated types
+
+	pass
